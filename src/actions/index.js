9f32e3afec10c9a1d4cd467344a45793fc23422c
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-09 11:27:33 -0800

    finalizing postsReducer with switch statement

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () =>  async dispatch => {
 const promise = await jsonPlaceholder.get('/posts');
 
-dispatch({ type: 'FETCH_POSTS', payload: response })
+dispatch({ type: 'FETCH_POSTS', payload: promise })
 };
 
 
